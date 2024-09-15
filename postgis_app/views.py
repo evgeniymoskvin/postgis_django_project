05dc4555f6a5b4b4ac4a6efefef81db00ae92dac
Evgeniy <evgeni.moskvin@gmail.com>
2024-09-15 12:39:33 +0300

    upd requirements.txt

--- postgis_app/views.py
+++ postgis_app/views.py
@@ -1,4 +1,6 @@
 import json
+import logging
+
 
 import folium
 from django.http import HttpResponse
@@ -8,15 +10,16 @@ from django.contrib.gis.geos import Polygon
 
 from .models import PolygonsModel
 
-
+logging.basicConfig(level=logging.INFO, filename="py_log.log", filemode="w")
 # Create your views here.
+
 
 class AboutView(View):
     """Приветственная страница"""
 
     def get(self, request):
         content = {}
-        return render(request, 'postgis_app/about.html', content)
+        return render(request, "postgis_app/about.html", content)
 
 
 class IndexView(View):
@@ -24,16 +27,13 @@ class IndexView(View):
 
     def get(self, request):
         content = {}
-        return render(request, 'postgis_app/index.html', content)
+        return render(request, "postgis_app/index.html", content)
 
     def post(self, request):
-        print(f'request.POST: {request.POST}')
-        send_text = str(request.POST.get('sendText'))
+        logging.info(f"request.POST: {request.POST}")
+        send_text = str(request.POST.get("sendText"))
         data_from_json = json.loads(send_text)
-        print(f'data_from_json: {data_from_json}')
-        print(f'data_from_json_type: {type(data_from_json)}')
-        name_polygon = str(request.POST.get('namePolygon'))
-        print(f'namePolygon: {type(name_polygon)}')
+        name_polygon = str(request.POST.get("namePolygon"))
         # Список координат для внесения в бд
         result_list = []
         antimeridian = False
@@ -54,12 +54,14 @@ class IndexView(View):
             else:
                 longitude = float(0)
             result_list.append((latitude, longitude))
-        print(result_list)
+        logging.info(f"result_list: {result_list}")
         # Для построения полигона необходимо 4 точки, а также чтобы 1 и последняя были равного значения
         if len(result_list) == 3 or result_list[0] != result_list[-1]:
             result_list.append(result_list[0])
         polygon = Polygon(result_list)
-        new_polygon = PolygonsModel(name_of_polygon=name_polygon, polygon=polygon, antimeridian=antimeridian)
+        new_polygon = PolygonsModel(
+            name_of_polygon=name_polygon, polygon=polygon, antimeridian=antimeridian
+        )
         new_polygon.save()
         # Возвращаем id записи, для редиректа на стороне клиента
         return HttpResponse(new_polygon.id)
@@ -69,11 +71,11 @@ class ShowListPolygonsView(View):
     """Отображения списка всех полигонов хранящихся в бд"""
 
     def get(self, request):
-        polygons = PolygonsModel.objects.get_queryset().order_by('id')
+        polygons = PolygonsModel.objects.get_queryset().order_by("id")
         content = {
-            'polygons': polygons,
+            "polygons": polygons,
         }
-        return render(request, 'postgis_app/all_polygons.html', content)
+        return render(request, "postgis_app/all_polygons.html", content)
 
 
 class ShowOnMapView(View):
@@ -81,29 +83,29 @@ class ShowOnMapView(View):
 
     def get(self, request, pk):
         polygon_data = PolygonsModel.objects.get(id=pk)
-        print(polygon_data.polygon)
         # Получение списка координат
         points = polygon_data.polygon[0]
         result_polygon = []
-
+        # Формируем список координат для folium
         for i in range(len(points)):
             result_polygon.append((points.y[i], points.x[i]))
-        print(result_polygon)
 
         map_details = folium.Map(location=(0, 0), zoom_start=2)
-        folium.PolyLine(result_polygon, color='red', fillcolor='red').add_to(map_details)
+        folium.PolyLine(result_polygon, color="red", fillcolor="red").add_to(
+            map_details
+        )
         folium.LayerControl().add_to(map_details)
         map_details = map_details._repr_html_()
         content = {
-            'polygon_name': polygon_data.name_of_polygon,
-            'polygon_id': polygon_data.id,
-            'polygon_antimeridian': polygon_data.antimeridian,
-            'map_details': map_details
+            "polygon_name": polygon_data.name_of_polygon,
+            "polygon_id": polygon_data.id,
+            "polygon_antimeridian": polygon_data.antimeridian,
+            "map_details": map_details,
         }
-        return render(request, 'postgis_app/on_map.html', content)
+        return render(request, "postgis_app/on_map.html", content)
 
     def post(self, request, pk):
         """Удаление полигона"""
         polygon_data = PolygonsModel.objects.get(id=pk)
         polygon_data.delete()
-        return redirect('all-polygons')
+        return redirect("all-polygons")
